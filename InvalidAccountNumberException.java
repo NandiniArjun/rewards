e126214483660065276ed297fb1adc10ddd2bc02
NandiniArjun <30292911+NandiniArjun@users.noreply.github.com>
2018-06-04 22:24:55 +0100

    Delete InvalidAccountNumberException.java

--- InvalidAccountNumberException.java
+++
@@ -1,7 +0,0 @@
-package com.sky.customer;
-
-public class InvalidAccountNumberException extends Exception {
-    public InvalidAccountNumberException(String message) {
-        super(message);
-    }
-}
